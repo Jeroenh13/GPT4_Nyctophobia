56c25127d6e581e9a0996f39e03de27889808878
nick van der mullen <Nickvdmullen@gmail.com>
2015-06-04 13:20:03 +0200

    Level 1

---
+++ Nyctophobia/Assets/Trigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+
+	public AudioSource mySource;
+	public AudioClip myClip;
+	private int lastTime;
+
+	// Use this for initialization
+	void Start () {
+		lastTime = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastTime++;
+	}
+
+	void OnTriggerEnter()
+	{
+		if (lastTime > 300) {
+			mySource.PlayOneShot (myClip);
+			lastTime = 0;
+		}
+	}
+}
