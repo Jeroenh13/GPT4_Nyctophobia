135a227f9c9ebec0db3e78288d843d045ad68735
Jeroen <jeroenh13@live.nl>
2015-06-26 09:05:36 +0200

    FlashLight

--- Nyctophobia/Assets/Scripts/Flashlight.cs
+++ Nyctophobia/Assets/Scripts/Flashlight.cs
@@ -10,19 +10,61 @@ public class Flashlight : MonoBehaviour {
 	public Light MyLight;
 	public Vector3 forward;
 	public int time = 800;
+    public ThalmicHub hub;
 	private Pose _lastPose = Pose.Unknown;
     public GameObject Player;
     public int speed;
     public GameObject LightObject;
     public CharacterController controller;
     public GameObject directional;
+    private Quaternion _antiYaw = Quaternion.identity;
+    private float _referenceRoll = 0.0f;
+
+    private bool syncing = true;
 
 	void Update()
 	{
-        
 		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
-        LightObject.transform.rotation = thalmicMyo.transform.rotation;
+        bool updateReference = false;
+        if (thalmicMyo.pose != _lastPose)
+        {
+            _lastPose = thalmicMyo.pose;
 
+            if (thalmicMyo.pose == Pose.FingersSpread)
+            {
+                updateReference = true;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            updateReference = true;
+        }
+
+        if (updateReference)
+        {
+           _antiYaw = Quaternion.FromToRotation(
+                new Vector3(myo.transform.forward.x, 0, myo.transform.forward.z),
+                new Vector3(0, 0, 1)
+            );
+
+            Vector3 referenceZeroRoll = computeZeroRollVector(myo.transform.forward);
+            _referenceRoll = rollFromZero(referenceZeroRoll, myo.transform.forward, myo.transform.up);
+        }
+        Vector3 zeroRoll = computeZeroRollVector(myo.transform.forward);
+        float roll = rollFromZero(zeroRoll, myo.transform.forward, myo.transform.up);
+        float relativeRoll = normalizeAngle(roll - _referenceRoll);
+        Quaternion antiRoll = Quaternion.AngleAxis(relativeRoll, myo.transform.forward);
+        thalmicMyo.transform.rotation = _antiYaw * antiRoll * Quaternion.LookRotation(myo.transform.forward);
+
+        if (thalmicMyo.xDirection == Thalmic.Myo.XDirection.TowardWrist)
+        {
+            thalmicMyo.transform.rotation = new Quaternion(transform.localRotation.x,
+                                                -transform.localRotation.y,
+                                                transform.localRotation.z,
+                                                -transform.localRotation.w);
+        }
+
+        LightObject.transform.rotation = thalmicMyo.transform.rotation;
 		//InvokeRepeating("Repeat", 1, 1);
 		InvokeRepeating ("Repeat", 0, 10);
 
@@ -104,4 +146,37 @@ public class Flashlight : MonoBehaviour {
 		time -= 1;
 	}
 
+    Vector3 computeZeroRollVector(Vector3 forward)
+    {
+        Vector3 antigravity = Vector3.up;
+        Vector3 m = Vector3.Cross(myo.transform.forward, antigravity);
+        Vector3 roll = Vector3.Cross(m, myo.transform.forward);
+
+        return roll.normalized;
+    }
+
+    // Adjust the provided angle to be within a -180 to 180.
+    float normalizeAngle(float angle)
+    {
+        if (angle > 180.0f)
+        {
+            return angle - 360.0f;
+        }
+        if (angle < -180.0f)
+        {
+            return angle + 360.0f;
+        }
+        return angle;
+    }
+    float rollFromZero(Vector3 zeroRoll, Vector3 forward, Vector3 up)
+    {
+        float cosine = Vector3.Dot(up, zeroRoll);
+
+        Vector3 cp = Vector3.Cross(up, zeroRoll);
+        float directionCosine = Vector3.Dot(forward, cp);
+        float sign = directionCosine < 0.0f ? 1.0f : -1.0f;
+
+        // Return the angle of roll (in degrees) from the cosine and the sign.
+        return sign * Mathf.Rad2Deg * Mathf.Acos(cosine);
+    }
 }
