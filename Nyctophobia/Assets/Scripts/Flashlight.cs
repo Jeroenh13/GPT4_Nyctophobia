29825c8770fc1b91d3eb2cafa3f123bd3f82b3b0
Jeroen <jeroenh13@live.nl>
2015-06-30 09:41:53 +0200

    movement

--- Nyctophobia/Assets/Scripts/Flashlight.cs
+++ Nyctophobia/Assets/Scripts/Flashlight.cs
@@ -7,18 +7,18 @@ using VibrationType = Thalmic.Myo.VibrationType;
 public class Flashlight : MonoBehaviour {
 
 	public GameObject myo = null;
-	public Light MyLight;
+	public Light SpotLight;
 	public Vector3 forward;
 	public int time = 800;
     public ThalmicHub hub;
 	private Pose _lastPose = Pose.Unknown;
     public GameObject Player;
     public int speed;
-    public GameObject LightObject;
     public CharacterController controller;
     public GameObject directional;
     private Quaternion _antiYaw = Quaternion.identity;
     private float _referenceRoll = 0.0f;
+    private float angleChecker = 0;
 
     private bool syncing = true;
 
@@ -30,32 +30,16 @@ public class Flashlight : MonoBehaviour {
         {
             _lastPose = thalmicMyo.pose;
 
-            if (thalmicMyo.pose == Pose.FingersSpread)
+            if (thalmicMyo.pose == Pose.DoubleTap)
             {
                 updateReference = true;
             }
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.O))
         {
             updateReference = true;
         }
-
-        if (updateReference)
-        {
-           _antiYaw = Quaternion.FromToRotation(
-                new Vector3(myo.transform.forward.x, 0, myo.transform.forward.z),
-                new Vector3(0, 0, 1)
-            );
-
-            Vector3 referenceZeroRoll = computeZeroRollVector(myo.transform.forward);
-            _referenceRoll = rollFromZero(referenceZeroRoll, myo.transform.forward, myo.transform.up);
-        }
-        Vector3 zeroRoll = computeZeroRollVector(myo.transform.forward);
-        float roll = rollFromZero(zeroRoll, myo.transform.forward, myo.transform.up);
-        float relativeRoll = normalizeAngle(roll - _referenceRoll);
-        Quaternion antiRoll = Quaternion.AngleAxis(relativeRoll, myo.transform.forward);
-        thalmicMyo.transform.rotation = _antiYaw * antiRoll * Quaternion.LookRotation(myo.transform.forward);
-
+        
         if (thalmicMyo.xDirection == Thalmic.Myo.XDirection.TowardWrist)
         {
             thalmicMyo.transform.rotation = new Quaternion(transform.localRotation.x,
@@ -64,28 +48,31 @@ public class Flashlight : MonoBehaviour {
                                                 -transform.localRotation.w);
         }
 
-        LightObject.transform.rotation = thalmicMyo.transform.rotation;
+        angleChecker = thalmicMyo.transform.eulerAngles.y;
+
+        
 		//InvokeRepeating("Repeat", 1, 1);
 		InvokeRepeating ("Repeat", 0, 10);
 
 		if (time % 100 == 0 ){
 			//MyLight.intensity -= 1;
 		}
-		if (MyLight.intensity == 0) {
+		if (SpotLight.intensity == 0) {
 			CancelInvoke("Repeat");
 			//MyLight.intensity = 8;
 		}
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || ((angleChecker <= 335 && angleChecker >= 270) && thalmicMyo.pose == Pose.FingersSpread))
         {
-            transform.Rotate(0, Input.GetAxis("Horizontal") / 0.5f, 0);
+            Player.transform.Rotate(0, Player.transform.position.y -3f, 0);
+            _antiYaw = Quaternion.FromToRotation(new Vector3(0, Player.transform.position.y -3, 0), new Vector3(0, 0, 1));
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || ((angleChecker >= 45 && angleChecker <= 90) && thalmicMyo.pose == Pose.FingersSpread))
         {
-            transform.Rotate(0, Input.GetAxis("Horizontal") * 4, 0);
+            Player.transform.Rotate(0, Player.transform.position.y + 1f, 0);
+            _antiYaw = Quaternion.FromToRotation(new Vector3(0, Player.transform.position.y + 1, 0), new Vector3(0, 0, 1));
        }
         if (Input.GetKey(KeyCode.W))
         {
-            
             forward = Player.transform.TransformDirection(Vector3.forward);
             controller.SimpleMove(forward);
         }
@@ -103,6 +90,24 @@ public class Flashlight : MonoBehaviour {
 		if (thalmicMyo.pose != _lastPose) {
 			_lastPose = thalmicMyo.pose;
 		}
+
+        if (updateReference)
+        {
+            _antiYaw = Quaternion.FromToRotation(
+                 new Vector3(myo.transform.forward.x, 0, myo.transform.forward.z),
+                 new Vector3(0, 0, 1)
+             );
+
+            Vector3 referenceZeroRoll = computeZeroRollVector(myo.transform.forward);
+            _referenceRoll = rollFromZero(referenceZeroRoll, myo.transform.forward, myo.transform.up);
+        }
+            Vector3 zeroRoll = computeZeroRollVector(myo.transform.forward);
+            float roll = rollFromZero(zeroRoll, myo.transform.forward, myo.transform.up);
+            float relativeRoll = normalizeAngle(roll - _referenceRoll);
+            Quaternion antiRoll = Quaternion.AngleAxis(relativeRoll, myo.transform.forward);
+            thalmicMyo.transform.rotation = _antiYaw * antiRoll * Quaternion.LookRotation(myo.transform.forward);
+            SpotLight.transform.rotation = thalmicMyo.transform.rotation;
+        
 			/*
 
 			if (Input.GetKey (KeyCode.D)) {
